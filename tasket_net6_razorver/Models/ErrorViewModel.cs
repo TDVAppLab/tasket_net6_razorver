580be00d52094437f679ff66d57127739c4c45fa
START PG <kngladsub@outlook.com>
2023-01-26 18:45:17 +0900

    commit_01
    - プロジェクトの生成に関わるコマンド
    >dotnet new mvc --auth Individual --use-local-db
    >dotnet sln add tasket_net6_razorver.csproj
    - LocalDBの生成に関わるコマンド
    >dotnet ef database update

---
+++ tasket_net6_razorver/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tasket_net6_razorver.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
